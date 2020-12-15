7634635153487f0d4c20014890ce79b4b2adb339
shivam26072006 <73473655+shivam26072006@users.noreply.github.com>
2020-12-15 18:00:13 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -3,14 +3,19 @@ class Bird extends Base{
 
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png") ;
-       
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+
 
     }
 
     display(){
-        this.body.position.x = mouseX ;
-        this.body.position.y = mouseY ;
+         
         super.display();
+        var position = [this.body.position.x,this.body.position.y];
+        this.trajectory.push(position);
+
+        for(var i = 0; i<)
     
     }
 
